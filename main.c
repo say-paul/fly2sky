c68fabf6ddd84340c9b9cbc26b75751472ea258b
Sayan Paul <saypaul@redhat.com>
2021-08-26 16:09:52 +0530

    adding SDK capabilities

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "pico/stdlib.h"
+int main (){
+    const uint led_pin = 25;
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin,GPIO_OUT);
+    while(true) {
+        gpio_put(led_pin,true);
+        sleep_ms(1000);
+        gpio_put(led_pin,false);
+        sleep_ms(1000);
+    }
+}
